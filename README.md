ad4afb3c94f209403b13c34d24a8b344f89190bc
Q1000Q <1000q.mj@gmail.com>
2024-06-08 17:09:35 +0200

    Added current commands section to README.md and commented main.rs

--- README.md
+++ README.md
@@ -7,3 +7,19 @@ This is BrainFuck interpreter written in rust. The 1st version (or possibly more
 ```
 .\BrainRust.exe .\code.bf
 ```
+
+## Current commands
+```
++ : Increments the value at the current cell by one
+- : Decrements the value at the current cell by one
+> : Moves the data pointer to the next cell (cell on the right)
+< : Moves the data pointer to the previous cell (cell on the left)
+. : Prints the ASCII value at the current cell (i.e. 65 = 'A')
+, : Reads a single input character into the current cell
+[ : If the value at the current cell is zero, skips to the corresponding ]
+    Otherwise, move to the next instruction
+] : If the value at the current cell is zero, move to the next instruction.
+    Otherwise, move backwards in the instructions to the corresponding [
+
+[ and ] form a while loop. Obviously, they must be balanced.
+```
--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,26 @@ use std::fs;
 use std::io;
 
 fn main() {
+    // Reads BrainFuck code file from argument
     let args: Vec<String> = env::args().collect();
     let file_path: &String = &args[1];
-
     let contents = fs::read_to_string(file_path)
         .expect("Should have been able to read file");
-
+    // Creates tape and pointer
     let mut tape: [u8; 30000] = [0; 30000];
     let mut pointer: usize = 0;
-    
+    // Loops through whole code
     let mut pc: usize = 0;
     while pc < contents.len() {
         match contents.as_bytes()[pc] {
+            // '>' adds 1 to pointer (moves pointer 1 right)
             b'>' => {
                 pointer += 1;
                 if pointer >= tape.len() {
                     pointer = 0;
                 }
             },
+            // '<' removes 1 to pointer (moves pointer 1 left)
             b'<' => {
                 if pointer == 0 {
                     pointer = tape.len() - 1;
@@ -28,9 +30,13 @@ fn main() {
                     pointer -= 1;
                 }
             },
+            // '+' adds 1 to localization at tape, that pointer points
             b'+' => tape[pointer] = tape[pointer].wrapping_add(1),
+            // '-' removes 1 to localization at tape, that pointer points
             b'-' => tape[pointer] = tape[pointer].wrapping_sub(1),
+            // '.' prints out content of tape cell that pointer is pointing (as a ASCII character)
             b'.' => print!("{}", tape[pointer] as char),
+            // ',' require user to enter a ASCII character and saves it to the tape cell that pointer is pointing
             b',' => {
                 let mut tmp = String::new();
                 io::stdin().read_line(&mut tmp).expect("Failed to read line");
@@ -38,6 +44,7 @@ fn main() {
                     tape[pointer] = byte;
                 }
             },
+            // '[' starts loop, if the value at the current cell is 0, then skips to the corresponding ']'. Otherwise, move to the next instruction
             b'[' => {
                 if tape[pointer] == 0 {
                     let mut loop_var: usize = 1;
@@ -54,6 +61,7 @@ fn main() {
                     }
                 }
             },
+            // ']' ends loop, f the value at the current cell is 0, move to the next instruction. Otherwise, move backwards in the instructions to the corresponding '['
             b']' => {
                 if tape[pointer] != 0 {
                     let mut loop_var: usize = 1;
