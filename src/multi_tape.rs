59e9d2a7e8b48c88543c48cf934335298154a3d8
Q1000Q <1000q.mj@gmail.com>
2026-02-24 16:49:17 +0100

    remove unnessesary variable

--- src/multi_tape.rs
+++ src/multi_tape.rs
@@ -3,9 +3,8 @@ use std::collections::HashMap;
 pub fn change_tape(pc: &mut usize, code_bytes: &[u8], tapes: &mut HashMap<u8, ([u8; 30000], usize)>, current_tape_id: &mut u8) {
     if *pc + 1 >= code_bytes.len() { return; }
     *pc += 1;
-    let new_tape = code_bytes[*pc];
 
-    *current_tape_id = new_tape;
+    *current_tape_id = code_bytes[*pc];
     if tapes.get(*&current_tape_id).is_none() {
         tapes.insert(*current_tape_id, ([0; 30000], 0));
     }
